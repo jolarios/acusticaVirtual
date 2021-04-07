3c9492623b5de293fb196735d87751c48642c6d6
Jorge <j.lareu@udc.es>
2021-04-07 14:06:11 +0200

    [SOFA] plotting utility for SOFA files

---
+++ sofa/proba_sofa.py
@@ -0,0 +1,42 @@
+from pysofaconventions import *
+import matplotlib.pyplot as plt
+import scipy.signal
+import soundfile as sf
+import numpy as np
+
+
+path = input("Introduce file's path (filename included): ")
+sofa = SOFAFile(path, 'r')
+print("\n")
+
+if(sofa.isValid()):
+	print("SOFA Convention:", sofa.getGlobalAttributeValue('SOFAConventions'))
+	print("\n")
+	print("Dimensions:")
+	sofa.printSOFADimensions()
+
+	print("\n")
+	print("Variables")
+	sofa.printSOFAVariables()
+
+
+	sourcePositions = sofa.getVariableValue('SourcePosition')
+
+	m = 0
+	print("\n")
+	print("Source Position of measurement " + str(m))
+	print(sourcePositions[m])
+
+
+	data = sofa.getDataIR()
+	hrtf = data[m,:,:]
+	
+	print("\n")
+	print("HRTF dimensions")
+	print(hrtf.shape)
+
+	plt.plot(hrtf[0], label="left", linewidth=0.5,  marker='o', markersize=1)
+	plt.plot(hrtf[1], label="right", linewidth=0.5,  marker='o', markersize=1)
+	plt.grid()
+	plt.legend()
+	plt.show()
