27b717a5ecd6d67abe94b11eddcde9db9ea5808e
Jorge <j.lareu@udc.es>
2021-04-15 12:25:22 +0200

    [WAV] first plot, only wave, not stereo

--- wave/waveProcessor.py
+++ wave/waveProcessor.py
@@ -1,8 +1,10 @@
+import matplotlib.pyplot as plt
+import numpy as np
 import wave
 
 #Realizar os inputs
 
-with wave.open('music.wav', mode='rb') as file:
+with wave.open('bird-1.wav', mode='rb') as file:
 	
 	print('\nInfo about: ')
 	info = file.getparams()
@@ -16,15 +18,26 @@ with wave.open('music.wav', mode='rb') as file:
 	right = []
 	left = []
 
-	for i in range(0, info[3]):
-		aux = file.readframes(1)
-		print(str(aux))
+	# Extract Raw Audio from Wav File
+	signal = file.readframes(-1)
+	signal = np.fromstring(signal, "Int16")
+	fs = file.getframerate()
+
+#	for i in range(0, info[3]):
+#		aux = file.readframes(1)
+#		print(str(aux))
 		
 		#print('aux: ' + str(aux) + ' [4:6]: ' + str(aux[4:6]) + 'hex: ' + str(int(aux[4:6], 16)))
 		#left.append(int(aux[4:6], 16))
 		#right.append(int(aux[4:6], 16))
+#	plt.plot(left, frames)
+#	plt.plot(right, frames)
 
-		
-	plt.plot(left, frames)
-	plt.plot(right, frames)
 
+
+	Time = np.linspace(0, len(signal) / fs, num=len(signal))
+	
+	plt.figure(1)
+	plt.title("Signal Wave...")
+	plt.plot(Time, signal)
+	plt.show()
